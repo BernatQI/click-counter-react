827e42de851afaf07c758b06de42c6ab4e24100c
BernatQi <bernatqipro@gmail.com>
2023-01-18 21:13:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import bernatQiLogoTransparent from './img/bernatqi-logo-tranparent.png';
+import Button from './components/Button';
+import Counter from './components/Counter';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manageClick = () => {
+    setNumClicks(numClicks + 33);
+  };
+  const resetCounter = () => {
+    setNumClicks(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='bernatqi-logo-container'>
+        <img
+          className='bernatqi-logo'
+          src={bernatQiLogoTransparent}
+          alt='BernatQi logo' />
+      </div>
+      <div className='main-container'>
+        <Counter
+          numClicks={numClicks} />
+        <Button
+          text='Click'
+          isClickButton={true}
+          manageClick={manageClick} />
+        <Button
+          text='Reset'
+          isClickButton={false}
+          manageClick={resetCounter} />
+      </div>
     </div>
   );
 }
