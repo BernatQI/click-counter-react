5eda0231033ffbdb96f3b3600d3dd897ddc8737f
BernatQi <bernatqipro@gmail.com>
2023-01-20 19:06:54 +0100

    Update to Classes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
+import React from 'react';
 import './App.css';
 import bernatQiLogoTransparent from './img/bernatqi-logo-tranparent.png';
 import Button from './components/Button';
 import Counter from './components/Counter';
-import { useState } from 'react';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numClicks: 0
+    };
+    this.manageClick = this.manageClick.bind(this);
+    this.resetCounter = this.resetCounter.bind(this);
+  }
 
-  const [numClicks, setNumClicks] = useState(0);
+  manageClick() {
+    this.setState(({ numClicks }) => ({ numClicks: numClicks + 1 }));
+  }
 
-  const manageClick = () => {
-    setNumClicks(numClicks + 1);
-  };
-  const resetCounter = () => {
-    setNumClicks(0);
-  };
+  resetCounter() {
+    this.setState({ numClicks: 0 });
+  }
 
-  return (
-    <div className="App">
-      <div className='bernatqi-logo-container'>
-        <img
-          className='bernatqi-logo'
-          src={bernatQiLogoTransparent}
-          alt='BernatQi logo' />
+  render() {
+    return (
+      <div className="App">
+        <div className='bernatqi-logo-container'>
+          <img
+            className='bernatqi-logo'
+            src={bernatQiLogoTransparent}
+            alt='BernatQi logo' />
+        </div>
+        <div className='main-container'>
+          <Counter
+            numClicks={this.state.numClicks} />
+          <Button
+            text='Click'
+            isClickButton={true}
+            manageClick={this.manageClick} />
+          <Button
+            text='Reset'
+            isClickButton={false}
+            manageClick={this.resetCounter} />
+        </div>
       </div>
-      <div className='main-container'>
-        <Counter
-          numClicks={numClicks} />
-        <Button
-          text='Click'
-          isClickButton={true}
-          manageClick={manageClick} />
-        <Button
-          text='Reset'
-          isClickButton={false}
-          manageClick={resetCounter} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,16 @@
 import React from "react";
 import '../stylesheets/Button.css';
 
-function Button({ text, isClickButton, manageClick }) {
-  return (
-    <button
-      className={isClickButton ? 'click-button' : 'reset-button'}
-      onClick={manageClick}>
-      {text}
-    </button>
-  )
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className={this.props.isClickButton ? 'click-button' : 'reset-button'}
+        onClick={this.props.manageClick}>
+        {this.props.text}
+      </button>
+    );
+  }
 }
 
 export default Button;
